5ac79d879dce40acb84cd0b4be7b0e5493ceaf23
abylch <abylch@hotmail.com>
2024-02-21 22:27:41 +0200

    yaml and bash updated

--- build.sh
+++ build.sh
@@ -12,3 +12,9 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 bash frontend-build.sh
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
