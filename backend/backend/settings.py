1731212534098177cb4a898e0156fc2073da99c6
abylch <abylch@hotmail.com>
2024-02-21 09:48:05 +0200

    render.yaml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -211,6 +211,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = 'static/images'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
